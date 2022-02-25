c939b6503755ba8d931f2419f0c8162113586a5a
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-25 16:15:01 +0100

    Mount  routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Import routes
+const bootcamps = require("./routes/bootcamps");
+
 // Load environment variables
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Monnt Routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
