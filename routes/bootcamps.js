94f26a5addea01fd4d01f6717acaa91b8fbf75f7
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-25 16:08:34 +0100

    Add methods in bootcamp routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,4 +1,33 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/api/v1/bootcamps", (req, res) => {
+	res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+router.put("/api/v1/bootcamps/:id", (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: `Get bootcamp ${req.params.id}`,
+	});
+});
+
+router.post("/api/v1/bootcamps", (req, res) => {
+	res.status(200).json({ success: true, msg: "Create new bootcamps" });
+});
+
+router.put("/api/v1/bootcamps/:id", (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: `Update bootcamp ${req.params.id}`,
+	});
+});
+
+router.delete("/api/v1/bootcamps/:id", (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: `Delete bootcamp ${req.params.id}`,
+	});
+});
+
 module.exports = router;
