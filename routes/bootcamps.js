05c725ccd11c2d538d635e0227649db1e4085277
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-25 16:44:49 +0100

    Mount `Bootcamp` routes to controllers

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,33 +1,20 @@
 const express = require("express");
+const {
+	getBootcamp,
+	getBootcamps,
+	createBootcamp,
+	updateBootcamp,
+	deleteBootcamp,
+} = require("../controllers/bootcamp");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
+router.route("/").get(getBootcamps).post(createBootcamp);
 
-router.put("/:id", (req, res) => {
-	res.status(200).json({
-		success: true,
-		msg: `Get bootcamp ${req.params.id}`,
-	});
-});
-
-router.post("/", (req, res) => {
-	res.status(200).json({ success: true, msg: "Create new bootcamps" });
-});
-
-router.put("/:id", (req, res) => {
-	res.status(200).json({
-		success: true,
-		msg: `Update bootcamp ${req.params.id}`,
-	});
-});
-
-router.delete("/:id", (req, res) => {
-	res.status(200).json({
-		success: true,
-		msg: `Delete bootcamp ${req.params.id}`,
-	});
-});
+router
+	.route("/:id")
+	.get(getBootcamp)
+	.put(updateBootcamp)
+	.delete(deleteBootcamp);
 
 module.exports = router;
