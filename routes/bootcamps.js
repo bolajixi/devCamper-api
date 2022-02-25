7dfcaa1dc8d001986bdff95782e5e6ac0b6c8244
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-25 16:12:19 +0100

    Modify  routes to match middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/api/v1/bootcamps", (req, res) => {
+router.get("/", (req, res) => {
 	res.status(200).json({ success: true, msg: "Show all bootcamps" });
 });
 
-router.put("/api/v1/bootcamps/:id", (req, res) => {
+router.put("/:id", (req, res) => {
 	res.status(200).json({
 		success: true,
 		msg: `Get bootcamp ${req.params.id}`,
 	});
 });
 
-router.post("/api/v1/bootcamps", (req, res) => {
+router.post("/", (req, res) => {
 	res.status(200).json({ success: true, msg: "Create new bootcamps" });
 });
 
-router.put("/api/v1/bootcamps/:id", (req, res) => {
+router.put("/:id", (req, res) => {
 	res.status(200).json({
 		success: true,
 		msg: `Update bootcamp ${req.params.id}`,
 	});
 });
 
-router.delete("/api/v1/bootcamps/:id", (req, res) => {
+router.delete("/:id", (req, res) => {
 	res.status(200).json({
 		success: true,
 		msg: `Delete bootcamp ${req.params.id}`,
