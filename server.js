bd56c291bb8310439f5a67367d24b13e168c4cf1
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-26 01:37:30 +0100

    Add global error handler

--- server.js
+++ server.js
@@ -18,12 +18,22 @@ if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
 
+app.use(express.json());
+
 // Monnt Routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+
+const server = app.listen(PORT, () => {
 	console.log(
 		`Server running in ${process.env.NODE_ENV} mode, Port: ${PORT}`
 	);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, Promise) => {
+	console.log(`Error: ${err.message}`);
+	// CLose server and exit process
+	server.close(() => process.exit(1));
 });
