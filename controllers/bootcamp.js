931494c33276a9f60abe2bf9b84cb3032a7a2839
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-26 01:38:10 +0100

    Add logic to create bootcamp

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -1,3 +1,5 @@
+const Bootcamp = require("../models/Bootcamp");
+
 // @desc    Get all bootcamps
 // @toute   GET /api/v1/bootcamps
 // @access  Public
@@ -21,8 +23,19 @@ exports.getBootcamp = (req, res, next) => {
 // @desc    Create new bootcamp
 // @toute   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res, next) => {
-	res.status(200).json({ success: true, msg: "Create new bootcamps" });
+exports.createBootcamp = async (req, res, next) => {
+	try {
+		const bootcamp = await Bootcamp.create(req.body);
+
+		res.status(201).json({
+			success: true,
+			data: bootcamp,
+		});
+	} catch (error) {
+		res.status(400).json({
+			success: false,
+		});
+	}
 };
 
 // @desc    Update single bootcamp
