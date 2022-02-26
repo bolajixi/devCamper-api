b5c3d4b5c8c7100b09539bf722a7a39bee13e5fb
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-26 22:25:38 +0100

    Add validation errors

--- middleware/error.js
+++ middleware/error.js
@@ -19,6 +19,12 @@ const errorHandler = (err, req, res, next) => {
 		error = new ErrorResponse(message, 400);
 	}
 
+	// Mogoose validation error
+	if (err.name === "ValidationError") {
+		const message = Object.values(err.errors).map((val) => val.message);
+		error = new ErrorResponse(message, 400);
+	}
+
 	res.status(error.StatusCode || 500).json({
 		success: false,
 		error: error.message || "Internal Server Error",
