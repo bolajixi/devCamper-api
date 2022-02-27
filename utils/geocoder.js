3f99ed621c431427e949b5200280f9a54e71e6f5
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-02-27 15:46:50 +0100

    Add geolocation

---
+++ utils/geocoder.js
@@ -0,0 +1,13 @@
+const NodeGeocoder = require("node-geocoder");
+
+const options = {
+	provider: process.env.GEOCODER_PROVIDER,
+	httpAdapter: "https",
+
+	apiKey: process.env.GEOCODER_API_KEY, // for Mapquest, OpenCage, Google Premier
+	formatter: null,
+};
+
+const geocoder = NodeGeocoder(options);
+
+module.exports = geocoder;
