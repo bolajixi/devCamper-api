832a93f45af6c12d9ed3de562201beb573bb17ae
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-03-05 13:22:09 +0100

    Reroute bootcamp route to courses route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,7 +8,13 @@ const {
 	getBootcampsInRadius,
 } = require("../controllers/bootcamp");
 
+// Include other resouce routers
+const courseRouter = require("./courses");
+
 const router = express.Router();
+
+// re route into other resource router
+router.use("/:bootcampId/courses", courseRouter);
 
 router.route("/radius/:zipcode/:distance").get(getBootcampsInRadius);
 
