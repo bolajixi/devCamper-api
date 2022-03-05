897baf18e1e1af47cd5f56fb7d36168db5cada1a
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-03-05 19:10:43 +0100

    Add aggregation to Bootcamp model

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,38 @@ const CourseSchema = new mongoose.Schema({
 	},
 });
 
+// Static method to get average of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+	console.log("Calculating Average Cost...");
+	const obj = await this.aggregate([
+		{
+			$match: { bootcamp: bootcampId },
+		},
+		{
+			$group: {
+				_id: "$bootcamp",
+				averageCost: { $avg: "$tuition" },
+			},
+		},
+	]);
+
+	try {
+		await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+			averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// Get average cost after save
+CourseSchema.post("save", function () {
+	this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Get average cost before remove
+CourseSchema.pre("remove", function () {
+	this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
