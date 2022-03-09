d9124e0f983c7286ee3fb050a1dd95c6a1083e59
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-03-09 09:27:23 +0100

    Fix typo in  schema

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -33,7 +33,7 @@ const BootcampSchema = new mongoose.Schema(
 			type: String,
 			match: [
 				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-				"Please ada avlid email",
+				"Please add a valid email",
 			],
 		},
 		address: {
