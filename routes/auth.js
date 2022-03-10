769b37b4ac95466d2654ec353104f9720a95d998
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-03-10 21:54:03 +0100

    Mount Login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { register } = require("../controllers/auth");
+const { register, login } = require("../controllers/auth");
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", register).post("/login", login);
 
 module.exports = router;
