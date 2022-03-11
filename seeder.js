280ff5b0e726c9c88a4521f26579d55ffdf2fd31
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-03-11 10:47:28 +0100

    Update seeder

--- seeder.js
+++ seeder.js
@@ -6,6 +6,7 @@ dotenv.config({ path: "./config/config.env" });
 
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/course");
+const User = require("./models/User");
 
 mongoose.connect(process.env.MONGO_URI, {
 	useNewUrlParser: true,
@@ -18,11 +19,15 @@ const bootcamps = JSON.parse(
 const courses = JSON.parse(
 	fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
+const users = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
+);
 
 const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps);
 		await Course.create(courses);
+		await User.create(users);
 
 		console.log("Data imported");
 		process.exit();
@@ -35,6 +40,7 @@ const deleteData = async () => {
 	try {
 		await Bootcamp.deleteMany();
 		await Course.deleteMany();
+		await User.deleteMany();
 
 		console.log("Data collection cleared");
 		process.exit(0);
