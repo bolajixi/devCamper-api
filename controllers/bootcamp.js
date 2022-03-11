1bcd4395f1ba170c8005a3bf4290fe42911c0ca7
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-03-11 10:56:45 +0100

    Add layer to prevent creating duplicate

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -33,6 +33,20 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @toute   POST /api/v1/bootcamps
 // @access  Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+	req.body.user = req.user;
+
+	// Add logic where Publisher can pbulish only one bootcamp
+	const publishedBootcamp = Bootcamp.findOne({ user: req.user.id });
+
+	if (publishedBootcamp && req.user.role !== "admin") {
+		return next(
+			new ErrorResponse(
+				`The user with ID ${req.user.id} has already published a bootcamp`,
+				400
+			)
+		);
+	}
+
 	const bootcamp = await Bootcamp.create(req.body);
 
 	res.status(201).json({
