abdb48cea895f135d839cdda80e1334e2a33dbd1
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-03-16 17:17:58 +0100

    Add update password route

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const {
 	login,
 	getMe,
 	forgotPassword,
+	updateDetails,
+	updatePassword,
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 const router = express.Router();
@@ -12,6 +14,8 @@ router
 	.post("/register", register)
 	.post("/login", login)
 	.get("/me", protect, getMe)
+	.put("/updatedetails", protect, updateDetails)
+	.put("/updatepassword", protect, updatePassword)
 	.post("/forgotpassword", forgotPassword);
 
 module.exports = router;
