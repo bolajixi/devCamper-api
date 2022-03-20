c0eeb944e9b126234084adf55b00b54be16a5113
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-03-20 20:08:31 +0100

    Add review aggregate to review model

--- models/Review.js
+++ models/Review.js
@@ -35,4 +35,38 @@ const ReviewSchema = new mongoose.Schema({
 
 // Prevent User from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
+
+// Static method to get average of average rating
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+	const obj = await this.aggregate([
+		{
+			$match: { bootcamp: bootcampId },
+		},
+		{
+			$group: {
+				_id: "$bootcamp",
+				averageRating: { $avg: "$rating" },
+			},
+		},
+	]);
+
+	try {
+		await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+			averageRating: obj[0].averageRating,
+		});
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// Get average cost after save
+ReviewSchema.post("save", function () {
+	this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Get average cost before remove
+ReviewSchema.pre("remove", function () {
+	this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model("Review", ReviewSchema);
