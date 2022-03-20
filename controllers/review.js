2de510c53423684a2d49606cdb7db54df2690287
Bolaji Olawale <bolaji.olawale007@gmail.com>
2022-03-20 20:43:05 +0100

    Add update and delete to review

--- controllers/review.js
+++ controllers/review.js
@@ -45,3 +45,83 @@ exports.getReview = asyncHandler(async (req, res, next) => {
 		data: review,
 	});
 });
+
+// @desc    Add a review
+// @route   POST /api/v1/bootcamps/:bootcampId/reviews
+// @access  Private
+
+exports.addReview = asyncHandler(async (req, res, next) => {
+	req.body.bootcamp = req.params.bootcampId;
+	req.body.user = req.user.id;
+
+	const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+	if (!bootcamp) {
+		return next(
+			new ErrorResponse(
+				`No Bootcamp with the ID of ${req.params.bootcampid}`,
+				404
+			)
+		);
+	}
+
+	const review = await Review.create(req.body);
+
+	res.status(200).json({
+		success: true,
+		data: review,
+	});
+});
+
+// @desc    Update a review
+// @route   PUT /api/v1/bootcamps/reviews/:id
+// @access  Private
+
+exports.updateReview = asyncHandler(async (req, res, next) => {
+	let review = await Review.findById(req.params.id);
+
+	if (!review) {
+		return next(
+			new ErrorResponse(`No Review with the ID of ${req.params.id}`, 404)
+		);
+	}
+
+	if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+		return next(new ErrorResponse(`No authorized to update review`, 401));
+	}
+
+	review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+		new: true,
+		runValidators: true,
+	});
+
+	res.status(200).json({
+		success: true,
+		data: review,
+	});
+});
+
+// @desc    Delete a review
+// @route   DELETE /api/v1/bootcamps/reviews/:id
+// @access  Private
+
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+	const review = await Review.findById(req.params.id);
+
+	if (!review) {
+		return next(
+			new ErrorResponse(`No Review with the ID of ${req.params.id}`, 404)
+		);
+	}
+
+	if (review.user.toString() !== req.user.id && req.user.role !== "admin") {
+		return next(new ErrorResponse(`No authorized to delete review`, 401));
+	}
+
+	await Review.remove();
+
+	res.status(200).json({
+		success: true,
+		data: {},
+	});
+});
--- routes/review.js
+++ routes/review.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { getReviews, getReview } = require("../controllers/review");
+const {
+	getReviews,
+	getReview,
+	addReview,
+	updateReview,
+	deleteReview,
+} = require("../controllers/review");
 
 const Review = require("../models/Review");
 const advancedResults = require("../middleware/advancedResults");
@@ -7,14 +13,21 @@ const { protect, authorize } = require("../middleware/auth");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(
-	advancedResults(Review, {
-		path: "bootcamp",
-		select: "name description",
-	}),
-	getReviews
-);
+router
+	.route("/")
+	.get(
+		advancedResults(Review, {
+			path: "bootcamp",
+			select: "name description",
+		}),
+		getReviews
+	)
+	.post(protect, authorize("user", "admin"), addReview);
 
-router.route("/:id").get(getReview);
+router
+	.route("/:id")
+	.get(getReview)
+	.put(protect, authorize("user", "admin"), updateReview)
+	.delete(protect, authorize("user", "admin"), deleteReview);
 
 module.exports = router;
